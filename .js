5c6e7e4b59301bbba0c87539e3e534d789bbf966
Amar07777 <dona87138@gmail.com>
2025-05-13 12:57:47 +0530

    Add files via upload

---
+++ .js
@@ -0,0 +1,8 @@
+function buyProduct(productName) {
+  alert(`Thank you for showing interest in ${productName}. Our team will contact you soon!`);
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  alert("Thank you for contacting us! We’ll get back to you shortly.");
+}
